ed8b4c2d47f327ee1053271d07f9b3c768f2791d
Jeffrey Hill <jeffrey.p.hill@nasa.gov>
2022-06-08 23:11:20 -0700

    Add run script for the NASA Pleiades super computer

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -N hello
+#PBS -q ts_production
+#PBS -l select=1:ncpus=20:model=ivy
+#PBS -l walltime=00:05:00
+#PBS -o run.log
+#PBS -j oe
+#PBS -k oed
+
+module purge
+module add mpi-hpe/mpt
+mpiexec -np 20 build_mpi/hello_mpi
